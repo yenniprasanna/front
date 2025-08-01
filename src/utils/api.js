8b408bbbaf81b96df6ce14334fae89e8474e7a6c
Mohammad Ramiz <ramizanas6@gmail.com>
2025-08-01 22:44:15 +0530

    final

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base configuration
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://backend-429v.onrender.com',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
